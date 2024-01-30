db946a1cb1e42c22ad5fa5ef8d8bebd762d7fe50
dhamaosta <viktor03.eriksson@gmail.com>
2024-01-30 16:26:47 +0100

    Kamera

---
+++ BestGame/Assets/Scripts/NavigationAndPlacementObstacleScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationAndPlacementObstacleScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        meshRenderer.enabled = false;
+
+    }
+    public MeshRenderer meshRenderer;
+
+
+    void Update()
+    {
+        
+    }
+}
