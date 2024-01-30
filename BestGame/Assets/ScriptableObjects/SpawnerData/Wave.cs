16aa1b580f937f981724919406db77c43b13e7a7
shittyGameDev <victor.berg1234@gmail.com>
2024-01-30 12:39:04 +0100

    Ops main branchen hehe

---
+++ BestGame/Assets/ScriptableObjects/SpawnerData/Wave.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "ScriptableObjects/Wave", order = 1)]
+public class Wave : ScriptableObject
+{
+    public SpawnerData spawnerData;
+}
+
--- BestGame/Assets/Scripts/WaveSpawner.cs
+++ BestGame/Assets/Scripts/WaveSpawner.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
-    public SpawnerData spawnerData;
-    private int currentSetIndex = 0;
+    public List<Wave> waves; // List to hold multiple waves
+    [SerializeField] private int currentWaveIndex = 0;
     [SerializeField] private int activeEnemies = 0;
     private float lastSpawnTime;
 
     void Start()
     {
-        StartCoroutine(SpawnWave());
+        if (waves.Count > 0)
+        {
+            StartCoroutine(SpawnWave(waves[currentWaveIndex]));
+        }
     }
 
-    IEnumerator SpawnWave()
+    IEnumerator SpawnWave(Wave wave)
     {
-        foreach (var set in spawnerData.enemySets)
+        foreach (var set in wave.spawnerData.enemySets)
         {
-            activeEnemies = set.amount;
-
             for (int i = 0; i < set.amount; i++)
             {
                 Vector3 randomOffset = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-5f, 5f));
@@ -28,19 +29,31 @@ public class WaveSpawner : MonoBehaviour
                 if (enemyScript != null)
                 {
                     enemyScript.OnDeath += OnEnemyDeath;
+                    activeEnemies++; // Increment activeEnemies for each enemy spawned
                     Debug.Log("Enemy script subscribed");
                 }
+
                 if (i == set.amount - 1) // If it's the last enemy of the set
                 {
                     lastSpawnTime = Time.time; // Update the last spawn time
                 }
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.1f); // Wait before spawning the next enemy
             }
 
-            yield return new WaitUntil(() => activeEnemies == 0 || TimePassedSinceLastSpawn(spawnerData.timeBetweenSets));
-            currentSetIndex++;
+            // Wait for either all enemies in the wave to be defeated or the time interval to pass before spawning the next set
+            yield return new WaitUntil(() => activeEnemies == 0 || TimePassedSinceLastSpawn(wave.spawnerData.timeBetweenSets));
+        }
+
+        // Wait for all enemies in the wave to be defeated before starting the next wave
+        yield return new WaitUntil(() => activeEnemies == 0);
+
+        currentWaveIndex++;
+        if (currentWaveIndex < waves.Count)
+        {
+            StartCoroutine(SpawnWave(waves[currentWaveIndex])); // Start the next wave
         }
     }
+
 
     private void OnEnemyDeath(Enemy enemy)
     {
